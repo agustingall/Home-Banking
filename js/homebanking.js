dc6c7bbda73e9f3c5ccf4bba80e426555dd29953
Agustín <agustingall@hotmail.com.ar>
2017-11-28 18:04:08 -0300

    Terminado

--- js/homebanking.js
+++ js/homebanking.js
@@ -2,13 +2,47 @@
 var nombreUsuario = "Agustín Galleguillo";
 var saldoCuenta = 10000;
 var limiteExtraccion = 2000;
+var servicio = {agua : 350,
+				telefono : 425,
+				luz : 210,
+				internet : 570
+}
+var cuentaAmiga = {NCuenta1 : 1234567,
+				   NCuenta2 : 7654321
+					 }
+var codSeguridad = 5678;
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML
+iniciarSesion();
 cargarNombreEnPantalla();
 actualizarSaldoEnPantalla();
 actualizarLimiteEnPantalla();
-
+ 
 //Funciones que tenes que completar
+function validar(R){
+	if (R == null  || isNaN(R) || R ==  0 ){
+		alert("El dato introducido es inválido.");
+		reload();
+	}
+	else{
+		return R;
+	}
+
+}
+
+function pagar(dinero,servicio){
+	if (dinero < saldoCuenta){
+		var dineroB = saldoCuenta;
+		saldoCuenta -= dinero;
+		alert("Has pagado el servicio " + servicio + ".\n Saldo anterior: $" + dineroB + "\nDinero descontado: $" + 
+			   dinero + "\nSaldo actual: $" + saldoCuenta);
+		actualizarSaldoEnPantalla();
+	}
+	else{
+		alert("No hay saldo disponible para realizar la operación.");
+	}
+}
+
 function haySaldoDisponible(dinero){
 	if(dinero < saldoCuenta){
 		return true;
@@ -19,7 +53,7 @@ function haySaldoDisponible(dinero){
 }
 
 function sePuedeExtraer(dinero){
-	if(dinero <= 2000){
+	if(dinero <= limiteExtraccion){
 		return true;
 	}
 	else{
@@ -40,11 +74,14 @@ function agregarSaldo(dinero){
 	saldoCuenta += dinero;
 }
 function cambiarLimiteDeExtraccion() {
+	var NLimite = validar(prompt("Ingrese el nuevo límite de extración"));
+	limiteExtraccion = NLimite;
+	actualizarLimiteEnPantalla();
 
 }
 
 function extraerDinero() {
-	dineroE = parseInt(prompt("Ingrese el dinero a extraer"));
+	var dineroE = validar(parseInt(prompt("Ingrese el dinero a extraer")));
 	var dineroB = saldoCuenta;
 	if (haySaldoDisponible(dineroE)){
 		if(sePuedeExtraer(dineroE)){
@@ -55,22 +92,25 @@ function extraerDinero() {
 				"\nSaldo actual: $" + saldoCuenta);
 			}
 			else{
-				alert("Solo se pueden extraer billetes de 100.")
+				alert("Solo se pueden extraer billetes de 100.");
 			}
 		}
 		else{
-			alert("La cantidad de dinero que deseas extraer es mayor a tu límite de extración.")
+			alert("La cantidad de dinero que deseas extraer es mayor a tu límite de extración.");
 		}
 	}
+	else if ( dineroE > saldoCuenta){
+		alert("No hay saldo disponible en tu cuenta para extraer esa cantidad de dinero");
+	}
 	else{
-		alert("No hay saldo disponible en tu cuenta para extraer esa cantidad de dinero")
+		alert("La característica ingresada no existe")
 	}
 	
 }
 
 function depositarDinero() {
 	var dineroB = saldoCuenta;
-	var dineroD = parseInt(prompt("Ingrese cuanto dinero va a depositar."));
+	var dineroD = validar(parseInt(prompt("Ingrese cuanto dinero va a depositar.")));
 	agregarSaldo(dineroD); 
 	actualizarSaldoEnPantalla();
 	alert("Saldo anterior: $" + dineroB +"\nDinero depositado: $" + dineroD + 
@@ -78,14 +118,56 @@ function depositarDinero() {
 }
 
 function pagarServicio() {
+	var eleccion = prompt("Ingrese el número que corresponda con el servicio que quieres pagar" + "\n1- Agua" +
+	 							   "\n2- Luz" + "\n3- Internet" + "\n4- Teléfono");
+
+	switch (eleccion) {
+		case 1:
+			pagar(servicio.agua, "Agua");
+			break;
+		case 2:
+			pagar(servicio.luz, "Luz");
+			break;
+		case 3:
+			pagar(servicio.internet, "Internet");
+			break;
+		case 4:
+			pagar(servicio.telefono, "Telefono");
+			break;
+		default:
+			alert("No existe el servicio que se ha seleccionado.");
+			break;
+	}
 
 }
 
 function transferirDinero() {
-
+	var monto = validar(parseInt(prompt("Ingrese el monto que desea transferir.")));
+	if(haySaldoDisponible(monto)){
+		var NCuenta = validar(parseInt(prompt("Ingrese el número de cuenta al que desea transferir el dinero.")));
+		if (NCuenta == cuentaAmiga.NCuenta1 || NCuenta == cuentaAmiga.NCuenta2){
+			saldoCuenta -= monto;
+			actualizarSaldoEnPantalla();
+			alert("Se han transferido $" + monto + "\nCuenta destino: " + NCuenta);
+		}
+		else{
+			alert("Solo puede transferir dinero a una cuenta amiga.");
+		}
+	}
 }
 
 function iniciarSesion() {
+
+	var codUser = prompt("Ingrese el código de su cuenta");
+	if (codUser == codSeguridad){
+		alert("Bienvenido/a Agustín Galleguillo ya puede comenzar a realizar operaciones ");
+	}
+	else{
+		alert("Código incorrecto. Tu dinero ha sido retenido por cuestiones de seguridad");
+		saldoCuenta = 0;
+		actualizarSaldoEnPantalla();
+	}
+
 
 }
 
